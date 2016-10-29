03cdfe228be074980efac4c7060369259a8f38f8
Yamagishi Kazutoshi <ykzts@desire.sh>
2016-10-30 01:40:23 +0900

    Copy cirlc.yml to build directory Close #27
    circle.ymlにgh-pagesブランチではCIを行わないように記述している。
    だがビルドしたものをgh-pagesブランチにpushする際に
    circle.ymlはコミットしていないために効力を発揮していなかった。
    
    ビルドスクリプトを実行する際にcircle.ymlを
    ビルドの成果物を入れているディレクトリーにコピーさせるように変更を入れる。

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,7 @@ REPO=$(git config remote.origin.url)
 cd $(dirname $0)/..
 NODE_ENV=production yarn run build
 cd build/public
+cp ../../circle.yml .
 sed -i'' -e 's/src="\//src=\".\//' index.html
 rm -rf .git
 git init .
@@ -14,6 +15,6 @@ git config user.name 'CicleCI'
 git config user.email 'sayhi@circleci.com'
 git remote add origin ${REPO}
 git checkout -b gh-pages
-git add index.html *.js
+git add index.html *.js circle.yml
 git commit -am 'add files'
 git push -f origin gh-pages
