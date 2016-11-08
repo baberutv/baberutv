4e9128aa70374c3daacd53fc7437bc52f041f114
Yamagishi Kazutoshi <ykzts@desire.sh>
2016-11-08 10:40:17 +0900

    Use flowtype Close #11 (#37)
    flowtypeを導入する。
    
    あくまで導入するだけであり、アノテーションをつけたファイルは限定的である。
    全てのファイルを静的型付けに対応させるのはおいおい進めて行きたい。

--- src/client.jsx
+++ src/client.jsx
@@ -1,8 +1,10 @@
+/* @flow */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-function insertCss(...styles) {
+function insertCss(...styles: Array<Object>): () => void {
   // eslint-disable-next-line no-underscore-dangle
   const removeStyles = styles.map(style => style._insertCss());
   return () => {
