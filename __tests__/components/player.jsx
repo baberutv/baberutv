79f1b1026544b0ba0c34b9ea1f0ae9cd6ac2df11
Yamagishi Kazutoshi <ykzts@desire.sh>
2016-11-12 19:35:27 +0900

    Add error handler for video component Close #38 (#106)
    `Video`コンポーネントにエラーハンドラーを追加する。
    
    動画を再生しようとしたタイミングでエラーが発生した場合に
    `alert`を使ってそれらしいメッセージの表示を行う。
    
    見た目を整えるという意味で`alert`はない選択肢ではあるが、
    ひとまずはこれで済ませる。
    将来的にはエラーはモーダルを表示させるようにしたい。

--- __tests__/components/player.jsx
+++ __tests__/components/player.jsx
@@ -5,23 +5,25 @@ import Player from '../../src/components/player';
 
 @provideContext({
   insertCss: PropTypes.func.isRequired,
+  setVideo: PropTypes.func.isRequired,
 })
 class MockPlayer extends Player {
 }
 
 function insertCss() {}
+function setVideo() {}
 
 test('mount', () => {
-  const player = mount(<MockPlayer context={{ insertCss }} />);
+  const player = mount(<MockPlayer context={{ insertCss, setVideo }} />);
   expect(player.find('.player').length).toBe(1);
 });
 
 test('aria-hidden=true', () => {
-  const player = mount(<MockPlayer context={{ insertCss }} />);
+  const player = mount(<MockPlayer context={{ insertCss, setVideo }} />);
   expect(player.find('.player[aria-hidden]').length).toBe(1);
 });
 
 test('aria-hidden=false', () => {
-  const player = mount(<MockPlayer context={{ insertCss }} src="https://example.com/index.m3u8" />);
+  const player = mount(<MockPlayer context={{ insertCss, setVideo }} src="https://example.com/index.m3u8" />);
   expect(player.find('.player[aria-hidden=false]').length).toBe(1);
 });
