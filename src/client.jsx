448186bfee625d08acbf8fabf266598b50e776af
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-02-24 17:06:06 +0900

    Remove Flow Close #193 (#198)

--- src/client.jsx
+++ src/client.jsx
@@ -1,11 +1,9 @@
-/* @flow */
-
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-function insertCss(...styles: Array<Object>): () => void {
+function insertCss(...styles) {
   // eslint-disable-next-line no-underscore-dangle
   const removeStyles = styles.map(style => style._insertCss());
   return () => {
