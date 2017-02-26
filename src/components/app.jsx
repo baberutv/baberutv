afd5ac86677eaa459fc74669b6a737d071d8afe1
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-02-26 23:18:09 +0900

    Styling using Material design Close #203 (#219)
    機能の実装を優先して急拵えの簡素なデザインにしていたが、使用の際にどうしても気になってしまっていた。
    自前でデザインを考えるのは楽しそうではあるが、門外漢であるためどうしても時間がかかってしまう。
    そのため簡単にできるようにスタイル用のフレームワークを使う。
    
    スマートフォンにも対応していて、かつReactに簡単に適用することができるMaterial-UIを採用する。
    
    Material-UIの現在の最新のバージョンはv0.17.0であるが、現在v1.0.0がalpha.6まで公開されている。
    Material-UI v1.0.0ではAPIに大きな変更が加えられている。
    v0.17.0を前提として書いたとしても、数箇月の内に出るであろうと予想されるv1.0.0で戸惑うことは必定である。
    軽く見る限りは充分プロダクションレディになっているように思えるためv1.0.0-alpha.6を前提にしてしまう。

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,10 @@
 import provideContext from 'context-provider/lib/provideContext';
-import dialogStyles from 'dialog-polyfill/dialog-polyfill.css';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React, { Component, PropTypes } from 'react';
 import URLSearchParams from 'url-search-params';
 import styles from '../styles/app.css';
 import Header from './header';
-import Modal from './modal';
 import Player from './player';
 
 function getQueryString() {
@@ -18,39 +17,26 @@ function getQueryString() {
 @provideContext({
   insertCss: PropTypes.func.isRequired,
 })
-@withStyles(dialogStyles, styles)
+@withStyles(styles)
 export default class App extends Component {
   static contextTypes = {
     insertCss: PropTypes.func.isRequired,
   };
 
   static childContextTypes = {
-    closeModal: PropTypes.func.isRequired,
-    editVideo: PropTypes.func.isRequired,
     setVideo: PropTypes.func.isRequired,
   };
 
   static displayName = 'App';
 
-  constructor(...args) {
-    super(...args);
-    this.handlePopState = this.handlePopState.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-    this.editVideo = this.editVideo.bind(this);
-    this.setVideo = this.setVideo.bind(this);
-  }
-
   state = {
-    editVideo: false,
+    open: false,
     videoUri: null,
   };
 
   getChildContext() {
-    const { closeModal, editVideo, setVideo } = this;
     return {
-      closeModal,
-      editVideo,
-      setVideo,
+      setVideo: this.setVideo,
     };
   }
 
@@ -62,7 +48,7 @@ export default class App extends Component {
       this.setState({ videoUri });
     } else {
       this.setState({
-        editVideo: true,
+        open: true,
       });
     }
   }
@@ -73,17 +59,17 @@ export default class App extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     return (
-      this.state.editVideo !== nextState.editVideo ||
+      this.state.open !== nextState.open ||
       this.state.videoUri !== nextState.videoUri
     );
   }
 
   componentWillUpdate(nextProps, nextState) {
-    const { editVideo, videoUri } = nextState;
+    const { open, videoUri } = nextState;
     if (this.state.videoUri !== videoUri) {
-      if (videoUri && editVideo) {
+      if (videoUri && open) {
         this.setState({
-          editVideo: false,
+          open: false,
         });
       }
       const currentUri = location.pathname + location.search;
@@ -98,42 +84,27 @@ export default class App extends Component {
     window.removeEventListener('popstate', this.handlePopState);
   }
 
-  setVideo({ uri }) {
+  setVideo = ({ uri }) => {
     this.setState({
       videoUri: uri,
     });
   }
 
-  editVideo() {
-    if (!this.state.editVideo) {
-      this.setState({
-        editVideo: true,
-      });
-    }
-  }
-
-  closeModal() {
-    if (this.state.editVideo) {
-      this.setState({
-        editVideo: false,
-      });
-    }
-  }
-
-  handlePopState({ state }) {
+  handlePopState = ({ state }) => {
     const { videoUri = '' } = state || {};
     this.setState({ videoUri });
   }
 
   render() {
     return (
-      <div className="app">
-        <Header videoUri={this.state.videoUri} />
-        <main>
-          <Player src={this.state.videoUri} />
-        </main>
-        <Modal open={this.state.editVideo} videoUri={this.state.videoUri} />
-      </div>
+      <MuiThemeProvider>
+        <div className="app">
+          <Header videoUri={this.state.videoUri} />
+          <main>
+            <Player src={this.state.videoUri} />
+          </main>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,89 +1,111 @@
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import AppBar from 'material-ui/AppBar';
+import Button from 'material-ui/Button';
+import { Dialog, DialogActions, DialogContent, DialogTitle } from 'material-ui/Dialog';
+import IconButton from 'material-ui/IconButton';
+import Text from 'material-ui/Text';
+import TextField from 'material-ui/TextField';
+import ToolBar from 'material-ui/Toolbar';
+import MenuIcon from 'material-ui/svg-icons/menu';
 import React, { Component, PropTypes } from 'react';
-import styles from '../styles/header.css';
 
-@withStyles(styles)
 export default class Header extends Component {
   static displayName = 'Header';
 
   static contextTypes = {
-    editVideo: PropTypes.func.isRequired,
+    setVideo: PropTypes.func.isRequired,
   };
 
   static propTypes = {
+    open: PropTypes.bool,
     videoUri: PropTypes.string,
   };
 
   static defaultProps = {
+    open: false,
     videoUri: null,
   };
 
   constructor(props, ...args) {
     super(props, ...args);
     Object.assign(this.state, {
-      visible: !props.videoUri,
+      open: props.open || !props.videoUri,
+      videoUri: props.videoUri || '',
     });
-    this.handleEditVideoButtonClick = this.handleEditVideoButtonClick.bind(this);
-    this.handleMouseEnter = this.handleMouseEnter.bind(this);
-    this.handleMouseLeave = this.handleMouseLeave.bind(this);
   }
 
   state = {
-    visible: false,
-  };
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.videoUri !== nextProps.videoUri) {
-      this.setState({
-        visible: !nextProps.videoUri,
-      });
-    }
+    open: false,
+    videoUri: null,
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return this.state.visible !== nextState.visible;
+    return (
+      this.state.open !== nextState.open ||
+      this.state.videoUri !== nextState.videoUri ||
+      this.props.open !== nextProps.open ||
+      this.props.videoUri !== nextProps.videoUri
+    );
   }
 
-  handleEditVideoButtonClick() {
-    this.context.editVideo();
+  componentWillUpdate(nextProps) {
+    if (nextProps.videoUri && this.props.videoUri !== nextProps.videoUri) {
+      this.setState({ open: false });
+    }
   }
 
-  handleMouseEnter() {
-    if (!this.state.visible) {
+  handleChange = ({ target }) => {
+    this.setState({
+      videoUri: target.value,
+    });
+  }
+
+  handleClick = () => {
+    if (!this.state.open) {
       this.setState({
-        visible: true,
+        open: true,
       });
     }
   }
 
-  handleMouseLeave() {
-    if (this.state.visible) {
-      this.setState({
-        visible: false,
-      });
-    }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.context.setVideo({
+      uri: this.state.videoUri,
+    });
+    return false;
   }
 
   render() {
     return (
-      <header
-        aria-hidden={!this.state.visible}
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-      >
-        <h1>TV</h1>
-        <nav>
-          <ul>
-            <li>
-              <button
-                onClick={this.handleEditVideoButtonClick}
-                type="button"
-              >
-                Open
-              </button>
-            </li>
-          </ul>
-        </nav>
+      <header>
+        <AppBar>
+          <ToolBar>
+            <IconButton contrast>
+              <MenuIcon onClick={this.handleClick} />
+            </IconButton>
+            <Text className="main-title" colorInherit type="title">TV</Text>
+            <Button contrast onClick={this.handleClick}>Open</Button>
+            <Dialog open={this.state.open}>
+              <form action="/" onSubmit={this.handleSubmit}>
+                <DialogTitle>Open video</DialogTitle>
+                <DialogContent>
+                  <TextField
+                    id="video-uri"
+                    inputProps={{ name: 'uri' }}
+                    label="URI"
+                    onChange={this.handleChange}
+                    required
+                    type="url"
+                    value={this.state.videoUri}
+                  />
+                </DialogContent>
+                <DialogActions>
+                  <Button primary type="submit">Play</Button>
+                </DialogActions>
+              </form>
+            </Dialog>
+          </ToolBar>
+        </AppBar>
       </header>
     );
   }
