99540c1dcdd3df41e1bdd1121e5fd3d52fd7337f
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-02-27 17:41:34 +0900

    Using CircleCI 2.0 Close #221
    CircleCI 2.0に移行させる。
    
    CircleCI 2.0はまだClosed Betaの段階ではあるが、検証を兼ねて使って行きたい。

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,12 @@ cd $(dirname $0)/..
 rm -rf build
 yarn build -- --env production
 cd build/public
-cp ../../circle.yml .
+cat <<EOF tee circle.yml
+general:
+  branches:
+    ignore:
+      - gh-pages
+EOF
 cat <<__EOS | node | tee CNAME
 const { parse: parseURL } = require('url');
 const { homepage: uri } = require('../../package.json');
